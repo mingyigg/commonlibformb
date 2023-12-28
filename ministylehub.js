b27283bd0777a4296e41cc9807017cde031e1dcf
明一gg <981552810@qq.com>
2023-12-28 12:20:46 +0800

    Update ministylehub.js

--- ministylehub.js
+++ ministylehub.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // 选择一个始终存在于 DOM 中的父元素，例如 'body'
     document.body.addEventListener('click', (e) => {
         // 检查点击的元素是否是您想要的 'a' 标签
-        if (e.target && e.target.matches('.your-class-name a')) {
+        if (e.target && e.target.matches('.sqs-block-instagram a')) {
             e.preventDefault(); // 阻止链接的默认动作
         }
     });
