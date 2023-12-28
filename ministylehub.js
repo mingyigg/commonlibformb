ec7f32381251bc8b2e5618ab3c87b1f3f4a70bee
明一gg <981552810@qq.com>
2023-12-28 12:34:41 +0800

    Update ministylehub.js

--- ministylehub.js
+++ ministylehub.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.body.addEventListener('click', (e) => {
         console.log("ministylehub 01");
         // 检查点击的元素是否是您想要的 'a' 标签
-        if (e.target && e.target.matches('.sqs-block-instagram a')) {
+        if (e.target && e.target.matches('.sqs-block-instagram img') || e.target.matches('.sqs-block-instagram a')) {
             console.log("ministylehub 02");
             e.preventDefault(); // 阻止链接的默认动作
             e.stopPropagation();
