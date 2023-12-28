5e61ed54b2d58ede9001d1b1621ed84f5a7d98df
明一gg <981552810@qq.com>
2023-12-28 12:17:51 +0800

    Update ministylehub.js

--- ministylehub.js
+++ ministylehub.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const divs = document.querySelectorAll('.sqs-block-instagram');
-    divs.forEach(div => {
-        const links = div.getElementsByTagName('a');
-        for (let link of links) {
-            link.addEventListener('click', (e) => {
-                e.preventDefault();
-            });
+    // 选择一个始终存在于 DOM 中的父元素，例如 'body'
+    document.body.addEventListener('click', (e) => {
+        // 检查点击的元素是否是您想要的 'a' 标签
+        if (e.target && e.target.matches('.your-class-name a')) {
+            e.preventDefault(); // 阻止链接的默认动作
         }
     });
 });
